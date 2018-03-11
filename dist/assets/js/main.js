83e8cc0c48c43edca4355973bd8e58ff22863751
Clauss Franco <you@example.com>
2018-03-11 12:02:15 -0600

    Footer

---
+++ dist/assets/js/main.js
@@ -0,0 +1,8 @@
+//llamamos  los  elementos
+
+var side =document.getElementById("talla").value;
+var centimetros = document.getElementById("centimetros");
+var pulgadas = document.getElementById("pulgadas");
+
+console.log(side);
+
